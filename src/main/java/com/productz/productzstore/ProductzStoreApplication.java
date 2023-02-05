687af8da932ad063bd21c96f472e2571beff7452
George <ignatov.g77@gmail.com>
2023-02-05 12:50:29 +0200

    initial commit

---
+++ src/main/java/com/productz/productzstore/ProductzStoreApplication.java
@@ -0,0 +1,13 @@
+package com.productz.productzstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductzStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductzStoreApplication.class, args);
+	}
+
+}
